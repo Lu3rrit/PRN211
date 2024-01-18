9f18e6d23feac2494e5b7def774fbdc91f389ccd
Linh <113653224+Lu3rrit@users.noreply.github.com>
2024-01-18 13:17:50 +0700

    Add files via upload

---
+++ AutomobileSolution/AutomobileWinApp/frmCarDetails.Designer.cs
@@ -0,0 +1,207 @@
+﻿using AutomobileLibrary.BussinessObject;
+using AutomobileLibrary.Repository;
+namespace AutomobileWinApp
+{
+    partial class frmCarDetails
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            IbCarID = new Label();
+            IbCarName = new Label();
+            IbManufacturer = new Label();
+            IbPrice = new Label();
+            IbReleaseYear = new Label();
+            txtCarID = new TextBox();
+            txtCarName = new TextBox();
+            txtPrice = new MaskedTextBox();
+            txtReleaseYear = new MaskedTextBox();
+            cboManufacturer = new ComboBox();
+            btnSave = new Button();
+            btnCancel = new Button();
+            fileSystemWatcher1 = new FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).BeginInit();
+            SuspendLayout();
+            // 
+            // IbCarID
+            // 
+            IbCarID.AutoSize = true;
+            IbCarID.Font = new Font("Segoe UI", 12F);
+            IbCarID.Location = new Point(162, 83);
+            IbCarID.Name = "IbCarID";
+            IbCarID.Size = new Size(53, 21);
+            IbCarID.TabIndex = 0;
+            IbCarID.Text = "Car ID";
+            // 
+            // IbCarName
+            // 
+            IbCarName.AutoSize = true;
+            IbCarName.Font = new Font("Segoe UI", 12F);
+            IbCarName.Location = new Point(162, 117);
+            IbCarName.Name = "IbCarName";
+            IbCarName.Size = new Size(80, 21);
+            IbCarName.TabIndex = 1;
+            IbCarName.Text = "Car Name";
+            // 
+            // IbManufacturer
+            // 
+            IbManufacturer.AutoSize = true;
+            IbManufacturer.Font = new Font("Segoe UI", 12F);
+            IbManufacturer.Location = new Point(162, 152);
+            IbManufacturer.Name = "IbManufacturer";
+            IbManufacturer.Size = new Size(104, 21);
+            IbManufacturer.TabIndex = 2;
+            IbManufacturer.Text = "Manufacturer";
+            // 
+            // IbPrice
+            // 
+            IbPrice.AutoSize = true;
+            IbPrice.Font = new Font("Segoe UI", 12F);
+            IbPrice.Location = new Point(162, 192);
+            IbPrice.Name = "IbPrice";
+            IbPrice.Size = new Size(44, 21);
+            IbPrice.TabIndex = 3;
+            IbPrice.Text = "Price";
+            // IbReleaseYear
+            // 
+            IbReleaseYear.AutoSize = true;
+            IbReleaseYear.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            IbReleaseYear.Location = new Point(162, 231);
+            IbReleaseYear.Name = "IbReleaseYear";
+            IbReleaseYear.Size = new Size(97, 21);
+            IbReleaseYear.TabIndex = 4;
+            IbReleaseYear.Text = "Release Year";
+            // 
+            // txtCarID
+            // 
+            txtCarID.Location = new Point(299, 83);
+            txtCarID.Name = "txtCarID";
+            txtCarID.Size = new Size(263, 23);
+            txtCarID.TabIndex = 5;
+            // 
+            // txtCarName
+            // 
+            txtCarName.Location = new Point(299, 117);
+            txtCarName.Name = "txtCarName";
+            txtCarName.Size = new Size(263, 23);
+            txtCarName.TabIndex = 6;
+            // 
+            // txtPrice
+            // 
+            txtPrice.Location = new Point(299, 192);
+            txtPrice.Mask = "000000000";
+            txtPrice.Name = "txtPrice";
+            txtPrice.Size = new Size(263, 23);
+            txtPrice.TabIndex = 7;
+            txtPrice.Text = "0";
+            // 
+            // txtReleaseYear
+            // 
+            txtReleaseYear.Location = new Point(299, 231);
+            txtReleaseYear.Mask = "0000";
+            txtReleaseYear.Name = "txtReleaseYear";
+            txtReleaseYear.Size = new Size(263, 23);
+            txtReleaseYear.TabIndex = 8;
+            txtReleaseYear.Text = "0";
+            // 
+            // cboManufacturer
+            // 
+            cboManufacturer.FormattingEnabled = true;
+            cboManufacturer.Items.AddRange(new object[] { "Audi", "BMW", "Ford", "Honda", "Hyundai", "Kia", "Suzuki", "Toyota" });
+            cboManufacturer.Location = new Point(299, 152);
+            cboManufacturer.Name = "cboManufacturer";
+            cboManufacturer.Size = new Size(263, 23);
+            cboManufacturer.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            btnSave.DialogResult = DialogResult.OK;
+            btnSave.Font = new Font("Segoe UI", 9F);
+            btnSave.Location = new Point(299, 296);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(95, 37);
+            btnSave.TabIndex = 10;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+          
+            // 
+            // btnCancel
+            // 
+            btnCancel.DialogResult = DialogResult.Cancel;
+            btnCancel.Location = new Point(458, 296);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(104, 39);
+            btnCancel.TabIndex = 11;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // fileSystemWatcher1
+            // 
+            fileSystemWatcher1.EnableRaisingEvents = true;
+            fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // frmCarDetails
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCancel);
+            Controls.Add(btnSave);
+            Controls.Add(cboManufacturer);
+            Controls.Add(txtReleaseYear);
+            Controls.Add(txtPrice);
+            Controls.Add(txtCarName);
+            Controls.Add(txtCarID);
+            Controls.Add(IbReleaseYear);
+            Controls.Add(IbPrice);
+            Controls.Add(IbManufacturer);
+            Controls.Add(IbCarName);
+            Controls.Add(IbCarID);
+            Name = "frmCarDetails";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frmCarDetails";
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label IbCarID;
+        private Label IbCarName;
+        private Label IbManufacturer;
+        private Label IbPrice;
+        private Label IbReleaseYear;
+        private TextBox txtCarID;
+        private TextBox txtCarName;
+        private MaskedTextBox txtPrice;
+        private MaskedTextBox txtReleaseYear;
+        private ComboBox cboManufacturer;
+        private Button btnSave;
+        private Button btnCancel;
+        private FileSystemWatcher fileSystemWatcher1;
+    }
+}
---
+++ AutomobileSolution/AutomobileWinApp/frmCarManagement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutomobileWinApp
+{
+    public partial class frmCarManagement : Form
+    {
+        public frmCarManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmCarManagement_Load(object sender, EventArgs e)
+        {
+            btnDelete.Enabled = false;
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
